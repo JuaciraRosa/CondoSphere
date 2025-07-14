8e05b0901aa4652cbbe87429b8e994b7b9aef0d5
JUACIRA ROSA <juacira.rosa@gmail.com>
2025-07-14 18:23:57 +0100

    Add project files.

---
+++ CondoSphere/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CondoSphere.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CondoSphereMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CondoSphereMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
